eba1667477488c43881bcbbdf93d945796bb12a3
minico <xj.zhang@msn.com>
2022-08-07 02:04:11 +0800

    return fake ipv6 address(::1) for AAAA request

--- dns2tcp.c
+++ dns2tcp.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
@@ -17,7 +18,9 @@
 #include "libev/ev.h"
 #undef _GNU_SOURCE
 
+
 #define DNS2TCP_VER "dns2tcp v1.1.0"
+
 
 #ifndef IPV6_V6ONLY
   #define IPV6_V6ONLY 26
@@ -35,16 +38,20 @@
   #define MSG_FASTOPEN 0x20000000
 #endif
 
+
 #define IP4STRLEN INET_ADDRSTRLEN /* ipv4addr max strlen */
 #define IP6STRLEN INET6_ADDRSTRLEN /* ipv6addr max strlen */
 #define PORTSTRLEN 6 /* "65535", include the null character */
 #define UDPDGRAM_MAXSIZ 1472 /* mtu:1500 - iphdr:20 - udphdr:8 */
 
+
 typedef uint16_t portno_t; /* 16bit */
 typedef struct sockaddr_in  skaddr4_t;
 typedef struct sockaddr_in6 skaddr6_t;
 
+
 #define IF_VERBOSE if (g_verbose)
+
 
 #define LOGINF(fmt, ...)                                                    \
     do {                                                                    \
@@ -55,6 +62,7 @@ typedef struct sockaddr_in6 skaddr6_t;
                 ##__VA_ARGS__);                                             \
     } while (0)
 
+
 #define LOGERR(fmt, ...)                                                    \
     do {                                                                    \
         struct tm *tm = localtime(&(time_t){time(NULL)});                   \
@@ -64,6 +72,66 @@ typedef struct sockaddr_in6 skaddr6_t;
                 ##__VA_ARGS__);                                             \
     } while (0)
 
+
+
+
+void hexdump(void *pSrc, int len) {
+    unsigned char *line;
+    int i;
+    int thisline;
+    int offset;
+
+
+    line = (unsigned char *)pSrc;
+    offset = 0;
+
+
+    while (offset < len) {
+        printf("%04x ", offset);
+        thisline = len - offset;
+
+
+
+
+        if (thisline > 16)
+        {
+            thisline = 16;
+        }
+
+
+
+
+        for (i = 0; i < thisline; i++)
+        {
+            printf("%02x ", line[i]);
+        }
+
+
+
+
+        for (; i < 16; i++)
+        {
+            printf("   ");
+        }
+
+
+
+
+        for (i = 0; i < thisline; i++)
+        {
+            printf("%c", (line[i] >= 0x20 && line[i] < 0x7f) ? line[i] : '.');
+        }
+
+
+
+
+        printf("\n");
+        offset += thisline;
+        line += thisline;
+    }
+}
+
+
 typedef struct {
     evio_t    watcher;
     uint8_t   buffer[2 + UDPDGRAM_MAXSIZ]; /* msglen(16bit) + msgbuf */
@@ -71,12 +139,15 @@ typedef struct {
     skaddr6_t srcaddr;
 } tcpwatcher_t;
 
+
 enum {
     OPT_IPV6_V6ONLY = 1 << 0,
     OPT_REUSE_PORT  = 1 << 1,
     OPT_QUICK_ACK   = 1 << 2,
     OPT_FAST_OPEN   = 1 << 3,
+    OPT_IPV4_ONLY   = 1 << 4,
 };
+
 
 static bool       g_verbose                 = false;
 static uint8_t    g_options                 = 0;
@@ -90,10 +161,12 @@ static portno_t   g_remote_portno           = 0;
 static skaddr6_t  g_remote_skaddr           = {0};
 static char       g_ipstr_buf[IP6STRLEN]    = {0};
 
+
 static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher, int events);
 static void tcp_connect_cb(evloop_t *evloop, evio_t *watcher, int events);
 static void tcp_sendmsg_cb(evloop_t *evloop, evio_t *watcher, int events);
 static void tcp_recvmsg_cb(evloop_t *evloop, evio_t *watcher, int events);
+
 
 static void set_nonblock(int sockfd) {
     int flags = fcntl(sockfd, F_GETFL, 0);
@@ -107,12 +180,14 @@ static void set_nonblock(int sockfd) {
     }
 }
 
+
 static void set_ipv6only(int sockfd) {
     if (setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &(int){1}, sizeof(int)) < 0) {
         LOGERR("[set_ipv6only] setsockopt(%d, IPV6_V6ONLY): (%d) %s", sockfd, errno, strerror(errno));
         exit(errno);
     }
 }
+
 
 static void set_reuseaddr(int sockfd) {
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0) {
@@ -121,12 +196,14 @@ static void set_reuseaddr(int sockfd) {
     }
 }
 
+
 static void set_reuseport(int sockfd) {
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int)) < 0) {
         LOGERR("[set_reuseport] setsockopt(%d, SO_REUSEPORT): (%d) %s", sockfd, errno, strerror(errno));
         exit(errno);
     }
 }
+
 
 static void set_nodelay(int sockfd) {
     if (setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &(int){1}, sizeof(int)) < 0) {
@@ -135,6 +212,7 @@ static void set_nodelay(int sockfd) {
     }
 }
 
+
 static void set_quickack(int sockfd) {
     if (setsockopt(sockfd, IPPROTO_TCP, TCP_QUICKACK, &(int){1}, sizeof(int)) < 0) {
         LOGERR("[set_quickack] setsockopt(%d, TCP_QUICKACK): (%d) %s", sockfd, errno, strerror(errno));
@@ -142,12 +220,14 @@ static void set_quickack(int sockfd) {
     }
 }
 
+
 static void set_syncnt(int sockfd, int syncnt) {
     if (setsockopt(sockfd, IPPROTO_TCP, TCP_SYNCNT, &syncnt, sizeof(syncnt)) < 0) {
         LOGERR("[set_syncnt] setsockopt(%d, TCP_SYNCNT): (%d) %s", sockfd, errno, strerror(errno));
         exit(errno);
     }
 }
+
 
 static int get_ipstr_family(const char *ipstr) {
     if (!ipstr) return -1; /* invalid */
@@ -160,6 +240,7 @@ static int get_ipstr_family(const char *ipstr) {
         return -1; /* invalid */
     }
 }
+
 
 static void build_socket_addr(int ipfamily, void *skaddr, const char *ipstr, portno_t portno) {
     if (ipfamily == AF_INET) {
@@ -175,6 +256,7 @@ static void build_socket_addr(int ipfamily, void *skaddr, const char *ipstr, por
     }
 }
 
+
 static void parse_socket_addr(const void *skaddr, char *ipstr, portno_t *portno) {
     if (((skaddr4_t *)skaddr)->sin_family == AF_INET) {
         const skaddr4_t *addr = skaddr;
@@ -187,11 +269,13 @@ static void parse_socket_addr(const void *skaddr, char *ipstr, portno_t *portno)
     }
 }
 
+
 static void print_command_help(void) {
     printf("usage: dns2tcp <-L listen> <-R remote> [-s syncnt] [-6rafvVh]\n"
            " -L <ip#port>            udp listen address, this is required\n"
            " -R <ip#port>            tcp remote address, this is required\n"
            " -s <syncnt>             set TCP_SYNCNT(max) for remote socket\n"
+           " -4                      request for type A(ipv4) only, ignore AAAA(ipv6) reqeust\n"
            " -6                      enable IPV6_V6ONLY for listen socket\n"
            " -r                      enable SO_REUSEPORT for listen socket\n"
            " -a                      enable TCP_QUICKACK for remote socket\n"
@@ -203,8 +287,11 @@ static void print_command_help(void) {
     );
 }
 
+
 static void parse_address_opt(char *ip_port_str, bool is_listen_addr) {
+    printf("[parse_address_opt]ip_port_str:%s is_listen_addr:%d\n", ip_port_str, is_listen_addr);
     const char *opt_name = is_listen_addr ? "listen" : "remote";
+
 
     char *portstr = strchr(ip_port_str, '#');
     if (!portstr) {
@@ -220,6 +307,7 @@ static void parse_address_opt(char *ip_port_str, bool is_listen_addr) {
         goto PRINT_HELP_AND_EXIT;
     }
 
+
     *portstr = 0; ++portstr;
     if (strlen(portstr) + 1 > PORTSTRLEN) {
         printf("[parse_address_opt] %s port is invalid: %s\n", opt_name, portstr);
@@ -230,6 +318,7 @@ static void parse_address_opt(char *ip_port_str, bool is_listen_addr) {
         printf("[parse_address_opt] %s port is invalid: %s\n", opt_name, portstr);
         goto PRINT_HELP_AND_EXIT;
     }
+
 
     const char *ipstr = ip_port_str;
     if (strlen(ipstr) + 1 > IP6STRLEN) {
@@ -242,6 +331,7 @@ static void parse_address_opt(char *ip_port_str, bool is_listen_addr) {
         goto PRINT_HELP_AND_EXIT;
     }
 
+
     if (is_listen_addr) {
         strcpy(g_listen_ipstr, ipstr);
         g_listen_portno = portno;
@@ -253,19 +343,23 @@ static void parse_address_opt(char *ip_port_str, bool is_listen_addr) {
     }
     return;
 
+
 PRINT_HELP_AND_EXIT:
     print_command_help();
     exit(1);
 }
 
+
 static void parse_command_args(int argc, char *argv[]) {
     char opt_listen_addr[IP6STRLEN + PORTSTRLEN] = {0};
     char opt_remote_addr[IP6STRLEN + PORTSTRLEN] = {0};
 
+
     opterr = 0;
     int shortopt = -1;
-    const char *optstr = "L:R:s:6rafvVh";
+    const char *optstr = "L:R:s:46rafvVh";
     while ((shortopt = getopt(argc, argv, optstr)) != -1) {
+        printf("[parse_command_args] shortopt: %d, optarg:%s\n", shortopt, optarg);
         switch (shortopt) {
             case 'L':
                 if (strlen(optarg) + 1 > IP6STRLEN + PORTSTRLEN) {
@@ -287,6 +381,9 @@ static void parse_command_args(int argc, char *argv[]) {
                     printf("[parse_command_args] invalid tcp syn cnt: %s\n", optarg);
                     goto PRINT_HELP_AND_EXIT;
                 }
+                break;
+            case '4':
+                g_options |= OPT_IPV4_ONLY;
                 break;
             case '6':
                 g_options |= OPT_IPV6_V6ONLY;
@@ -319,6 +416,7 @@ static void parse_command_args(int argc, char *argv[]) {
         }
     }
 
+
     if (strlen(opt_listen_addr) == 0) {
         printf("[parse_command_args] missing option: '-L'\n");
         goto PRINT_HELP_AND_EXIT;
@@ -328,28 +426,34 @@ static void parse_command_args(int argc, char *argv[]) {
         goto PRINT_HELP_AND_EXIT;
     }
 
+
     parse_address_opt(opt_listen_addr, true);
     parse_address_opt(opt_remote_addr, false);
     return;
+
 
 PRINT_HELP_AND_EXIT:
     print_command_help();
     exit(1);
 }
 
+
 int main(int argc, char *argv[]) {
     signal(SIGPIPE, SIG_IGN);
     setvbuf(stdout, NULL, _IOLBF, 256);
     parse_command_args(argc, argv);
 
+
     LOGINF("[main] udp listen addr: %s#%hu", g_listen_ipstr, g_listen_portno);
     LOGINF("[main] tcp remote addr: %s#%hu", g_remote_ipstr, g_remote_portno);
     if (g_syn_maxcnt) LOGINF("[main] enable TCP_SYNCNT:%hhu sockopt", g_syn_maxcnt);
+    if (g_options & OPT_IPV4_ONLY) LOGINF("[main] enable IPV4_ONLY");
     if (g_options & OPT_IPV6_V6ONLY) LOGINF("[main] enable IPV6_V6ONLY sockopt");
     if (g_options & OPT_REUSE_PORT) LOGINF("[main] enable SO_REUSEPORT sockopt");
     if (g_options & OPT_QUICK_ACK) LOGINF("[main] enable TCP_QUICKACK sockopt");
     if (g_options & OPT_FAST_OPEN) LOGINF("[main] enable TCP_FASTOPEN sockopt");
     IF_VERBOSE LOGINF("[main] verbose mode, affect performance");
+
 
     g_udp_sockfd = socket(g_listen_skaddr.sin6_family, SOCK_DGRAM, 0);
     if (g_udp_sockfd < 0) {
@@ -357,24 +461,29 @@ int main(int argc, char *argv[]) {
         return errno;
     }
 
+
     set_nonblock(g_udp_sockfd);
     set_reuseaddr(g_udp_sockfd);
     if (g_options & OPT_REUSE_PORT) set_reuseport(g_udp_sockfd);
     if ((g_options & OPT_IPV6_V6ONLY) && g_listen_skaddr.sin6_family == AF_INET6) set_ipv6only(g_udp_sockfd);
+
 
     if (bind(g_udp_sockfd, (void *)&g_listen_skaddr, g_listen_skaddr.sin6_family == AF_INET ? sizeof(skaddr4_t) : sizeof(skaddr6_t)) < 0) {
         LOGERR("[main] bind udp address: (%d) %s", errno, strerror(errno));
         return errno;
     }
 
+
     evloop_t *evloop = ev_default_loop(0);
     evio_t *watcher = &(evio_t){0};
     ev_io_init(watcher, udp_recvmsg_cb, g_udp_sockfd, EV_READ);
     ev_io_start(evloop, watcher);
 
+
     ev_run(evloop, 0);
     return 0;
 }
+
 
 static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher __attribute__((unused)), int events __attribute__((unused))) {
     tcpwatcher_t *tcpw = malloc(sizeof(*tcpw));
@@ -391,9 +500,26 @@ static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher __attribute__((unus
         LOGINF("[udp_recvmsg_cb] recv from %s#%hu, nrecv:%zd", g_ipstr_buf, portno, nrecv);
     }
     uint16_t *msglen_ptr = (void *)tcpw->buffer;
+
+#if 1
+    char domain[256] = {0};
+    strcpy(domain, (const char*)tcpw->buffer + 2 + 12);
+    char* ch = domain + 1;
+    while(*ch != '\0') {
+        if (!isprint(*ch)) {
+            *ch = '.';
+        }
+        ++ch;
+    }
+    uint16_t req_type = ntohs(*(uint16_t*)(tcpw->buffer + 2 + 12 + strlen(domain) + 1));
+    LOGINF("request dns for:%s with type:%s\n", domain, req_type == 1 ? "A" : "AAAA");
+#endif
+
     *msglen_ptr = htons(nrecv); /* msg length */
     nrecv += 2; /* msglen + msgbuf */
     tcpw->nrcvsnd = 0;
+
+    //hexdump(tcpw->buffer, nrecv);
 
     int sockfd = socket(g_remote_skaddr.sin6_family, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -405,6 +531,7 @@ static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher __attribute__((unus
     set_nodelay(sockfd);
     if (g_syn_maxcnt) set_syncnt(sockfd, g_syn_maxcnt);
     if (g_options & OPT_QUICK_ACK) set_quickack(sockfd);
+
 
     bool tfo_succ = false;
     if (g_options & OPT_FAST_OPEN) {
@@ -428,6 +555,7 @@ static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher __attribute__((unus
         IF_VERBOSE LOGINF("[udp_recvmsg_cb] try to connect to %s#%hu", g_remote_ipstr, g_remote_portno);
     }
 
+
     if (tfo_succ && tcpw->nrcvsnd >= nrecv) {
         tcpw->nrcvsnd = 0; /* reset to zero for recv data */
         ev_io_init((evio_t *)tcpw, tcp_recvmsg_cb, sockfd, EV_READ);
@@ -437,11 +565,13 @@ static void udp_recvmsg_cb(evloop_t *evloop, evio_t *watcher __attribute__((unus
     ev_io_start(evloop, (evio_t *)tcpw);
     return;
 
+
 CLOSE_TCP_SOCKFD:
     close(sockfd);
 FREE_TCP_WATCHER:
     free(tcpw);
 }
+
 
 static void tcp_connect_cb(evloop_t *evloop, evio_t *watcher, int events __attribute__((unused))) {
     if (getsockopt(watcher->fd, SOL_SOCKET, SO_ERROR, &errno, &(socklen_t){sizeof(errno)}) < 0 || errno) {
@@ -455,6 +585,7 @@ static void tcp_connect_cb(evloop_t *evloop, evio_t *watcher, int events __attri
     ev_set_cb(watcher, tcp_sendmsg_cb);
     ev_invoke(evloop, watcher, EV_WRITE);
 }
+
 
 static void tcp_sendmsg_cb(evloop_t *evloop, evio_t *watcher, int events __attribute__((unused))) {
     tcpwatcher_t *tcpw = (void *)watcher;
@@ -479,9 +610,11 @@ static void tcp_sendmsg_cb(evloop_t *evloop, evio_t *watcher, int events __attri
     }
 }
 
+
 static void tcp_recvmsg_cb(evloop_t *evloop, evio_t *watcher, int events __attribute__((unused))) {
     tcpwatcher_t *tcpw = (void *)watcher;
     void *buffer = tcpw->buffer;
+
 
     ssize_t nrecv = recv(watcher->fd, buffer + tcpw->nrcvsnd, 2 + UDPDGRAM_MAXSIZ - tcpw->nrcvsnd, 0);
     if (nrecv < 0) {
@@ -496,6 +629,38 @@ static void tcp_recvmsg_cb(evloop_t *evloop, evio_t *watcher, int events __attri
     tcpw->nrcvsnd += nrecv;
     IF_VERBOSE LOGINF("[tcp_recvmsg_cb] recv from %s#%hu, nrecv:%zd", g_remote_ipstr, g_remote_portno, nrecv);
     if (tcpw->nrcvsnd < 2 || tcpw->nrcvsnd < 2 + ntohs(*(uint16_t *)buffer)) return;
+
+    //hexdump(tcpw->buffer, nrecv);
+
+#if 1
+    char domain[256] = {0};
+    strcpy(domain, (const char*)tcpw->buffer + 2 + 12);
+    char* ch = domain + 1;
+    while(*ch != '\0') {
+        if (!isprint(*ch)) {
+            *ch = '.';
+        }
+        ++ch;
+    }
+    uint16_t req_type = ntohs(*(uint16_t*)(tcpw->buffer + 2 + 12 + strlen(domain) + 1));
+    LOGINF("response dns for:%s with type:%s\n", domain, req_type == 1 ? "A" : "AAAA");
+    uint16_t msg_len = ntohs(*(uint16_t *)buffer);
+    if ((g_options & OPT_IPV4_ONLY && req_type == 28) && tcpw->buffer[9] > 0) {
+        printf("ignore AAAA type response for:%s\n", domain);
+        tcpw->buffer[9] = 1;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 ] = 0;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 + 1] = 0x1c;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 + 2] = 0;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 + 2 + 1] = 1;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 + 4 + 4 + 1] = 16;
+        tcpw->buffer[2 + 12 + strlen(domain) + 1 + 4 + 2 + 4 + 4 + 2 + 15] = 1;
+        memset(tcpw->buffer + 2 + 12 + strlen(domain) + 1 + 4 + 2 + 4 + 4 + 2, 0, 15);
+        msg_len = 12 + strlen(domain) + 1 + 4 + 2 + 4 + 4 + 2 + 16;
+        tcpw->buffer[1] = msg_len;
+    }
+    //hexdump(tcpw->buffer + 2, msg_len);
+#endif
+
 
     const void *sendto_skaddr = &tcpw->srcaddr;
     socklen_t sendto_skaddrlen = tcpw->srcaddr.sin6_family == AF_INET ? sizeof(skaddr4_t) : sizeof(skaddr6_t);
